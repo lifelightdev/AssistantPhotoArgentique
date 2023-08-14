8fb72fa59fed61c29b92b2ce88a0e6e2c5c02025
life light <lifelightdev@gmail.com>
2023-08-14 21:51:00 +0200

    Ajouter un test

--- Back/apa/src/test/java/life/light/apa/priseDeVue/service/PriseDeVueServiceTest.java
+++ Back/apa/src/test/java/life/light/apa/priseDeVue/service/PriseDeVueServiceTest.java
@@ -16,7 +16,11 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.*;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -84,10 +88,49 @@ class PriseDeVueServiceTest {
         when(filmRepository.findById(idFilm)).thenReturn(Optional.of(film));
         when(statutVueRepository.findById(StatutVue.ARealiser)).thenReturn(Optional.of(statutVue));
 
-        Vue vue = service.ajouterUneVue(idPriseDeVue,idAppareilPhoto,idFilm);
+        Vue vue = service.ajouterUneVue(idPriseDeVue, idAppareilPhoto, idFilm);
 
         assertThat(vue.getFilm().getId()).isEqualTo(idFilm);
     }
+
+    @Test
+    void uneVueDejaRealiser() {
+        Long idFilm = 1L;
+        Long idPriseDeVue = 1L;
+        Long idAppareilPhoto = 1L;
+
+        PriseDeVue mockPriseDeVue = new PriseDeVue();
+        mockPriseDeVue.setId(idPriseDeVue);
+        mockPriseDeVue.setNom("Nom de la prise de vue");
+        Film film = new Film();
+        film.setId(idFilm);
+        Materiel materiel = new Materiel();
+        materiel.setNom("Nom de l'appareil photo");
+        AppareilPhoto appareilPhoto = new AppareilPhoto();
+        appareilPhoto.setId(idAppareilPhoto);
+        appareilPhoto.setMateriel(materiel);
+        StatutVue statutVue = new StatutVue();
+        statutVue.setId(StatutVue.ARealiser);
+        Vue mockVue = new Vue();
+        mockVue.setFilm(film);
+        mockVue.setPriseDeVue(mockPriseDeVue);
+        mockVue.setAppareilPhoto(appareilPhoto);
+
+        List<Vue> listVue = new ArrayList<>();
+        listVue.add(mockVue);
+
+        when(vueRepository.findVueARealiser()).thenReturn(listVue);
+
+        Exception exception = assertThrows(PriseDeVueException.class, () -> service.ajouterUneVue(idPriseDeVue, idAppareilPhoto, idFilm));
+
+        String expectedMessage = "Impossible d'ajouter une vue car il y a déjà une vue au statut 'A réaliser'";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+        assertThat(actualMessage).isEqualTo(expectedMessage);
+
+    }
+
     @Test
     void uneSeuleVueARealiser() {
         // Given
@@ -103,10 +146,5 @@ class PriseDeVueServiceTest {
     void listeDesFilmsDUnePriseDeVue() {
     }
 
-    @Test
-    void ajouterUneVueEnErreur() {
-        // Given
-        // When
-        // Then
-    }
+
 }
