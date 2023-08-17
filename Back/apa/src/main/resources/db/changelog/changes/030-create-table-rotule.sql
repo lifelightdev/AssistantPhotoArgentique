a3bfd1db4726b0ef2396aff085fd2a38560e67fc
life light <lifelightdev@gmail.com>
2023-08-17 11:23:09 +0200

    Correction du script de création de la table Rotule

--- Back/apa/src/main/resources/db/changelog/changes/030-create-table-rotule.sql
+++ Back/apa/src/main/resources/db/changelog/changes/030-create-table-rotule.sql
@@ -1,12 +1,11 @@
-create table if not exists apa.pied
+create table if not exists apa.rotule
 (
-    id               bigint auto_increment primary key,
-    materiel_id      bigint       null,
-    dimension_ouvert varchar(255) null,
-    dimension_ferme  varchar(255) null,
-    pas_de_vis       varchar(255) null,
-    rotule_id        bigint       null,
+    id                   bigint auto_increment primary key,
+    materiel_id          bigint               null,
+    pas_de_vis_materiel  varchar(255)         null,
+    pas_de_vis_pied      varchar(255)         null,
+    avec_fixation_rapide tinyint(1) default 0 null,
+    constraint avec_fixation_rapide_UNIQUE unique (avec_fixation_rapide),
     constraint id_UNIQUE unique (id),
-    constraint FK_PIED_ON_MATERIEL foreign key (materiel_id) references apa.materiel (id),
-    constraint FK_ROTULE_ON_ROTULE foreign key (rotule_id) references apa.rotule (id)
+    constraint FK_ROTULE_ON_MATERIEL foreign key (materiel_id) references materiel (id)
 );
